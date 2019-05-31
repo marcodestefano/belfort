ef591c9c6699a7a7508ef2ed7f148b6a8dab3cc8
Marco De Stefano <destmark@gmail.com>
2019-05-31 19:40:15 +0200

    Updated trading engine

--- belfort.py
+++ belfort.py
@@ -4,8 +4,17 @@ import cbpro
 import os
 import time
 from decimal import Decimal
+from datetime import datetime
+from datetime import timedelta
 
 ORDER_SIDE_BUY = "buy"
+ORDER_SIDE_SELL = "sell"
+
+BUY_PRICE_FACTOR = 0.5
+SELL_PRICE_FACTOR = 1.5
+
+BASE_CURRENCY = "EUR"
+CRYPTO_CURRENCY = "XLM"
 
 def getConfiguration():
     """Retrieve the configuration file."""
@@ -36,8 +45,9 @@ def promptCommand(text, acceptedValues):
 def getCurrencyPair(baseCurrency, otherCurrency):
 	return otherCurrency + "-" + baseCurrency
 
-def getWallets(accounts):
+def getWallets(client):
     """Retrieve the wallets of an account."""
+    accounts = client.get_accounts()
     wallets = {}
     if accounts:
         for account in accounts:
@@ -45,25 +55,26 @@ def getWallets(accounts):
                                     " " + account["currency"]
     return wallets
 
-def getWallet(accounts, currency):
+def getWallet(client, currency):
+	accounts = client.get_accounts()
 	if accounts:
 		for account in accounts:
 			if account["currency"] == currency:
 				return account["balance"]
 
-def printWallets(accounts):
+def printWallets(client):
     """Print wallet content."""
     print ("\nHere's the list of your wallets:\n")
-    wallets = getWallets(accounts)
-    print(wallets)
+    wallets = getWallets(client)
     for account in wallets:
-        print ("%s: %s" % (account, wallets[account]))
+    	if Decimal(wallets[account][:-4]) > 0:
+        	print ("%s: %s" % (account, wallets[account]))
 
 
 def printValues(client):
     """Print currency values."""
     currencies = ["BTC", "ETH", "LTC", "XLM"]
-    baseCurrency = "EUR"
+    baseCurrency = BASE_CURRENCY
     print ("\nHere the current values of cryptocurrencies:\n")
     for currency in currencies:
         currencyPair = getCurrencyPair(baseCurrency, currency)
@@ -71,8 +82,8 @@ def printValues(client):
         print ("Current %s price is: %s %s" % (currency, ticker["price"],
                                               baseCurrency))
 
-def getValue(client, baseCurrency, otherCurrency):
-	currencyPair = getCurrencyPair(baseCurrency, otherCurrency)
+def getValue(client, baseCurrency, cryptoCurrency):
+	currencyPair = getCurrencyPair(baseCurrency, cryptoCurrency)
 	ticker = client.get_product_ticker(product_id=currencyPair)
 	if ticker:
 		return ticker["price"]
@@ -80,44 +91,52 @@ def getValue(client, baseCurrency, otherCurrency):
 		return ""
 
 def cancelObsoleteOrders(client):
+	currencyPair = getCurrencyPair(BASE_CURRENCY, CRYPTO_CURRENCY)
+	orders = list(client.get_orders())
+	for order in orders:
+		if order["product_id"] == currencyPair :
+			placedAt = datetime.strptime(order["created_at"],'%Y-%m-%dT%H:%M:%S.%fZ')
+			limitTime = placedAt + timedelta(seconds = 10)
+			actualTime = datetime.utcnow()
+			if actualTime > limitTime:
+				client.cancel_order(order["id"])
+				print ("Canceled " + order["side"] + " order " + order["id"] + " of size " + order["size"] + " and price " + order["price"] + "placed at " + str(placedAt))
 	return
 
-def getEuroInOpenOrders(orders):
+def getEuroInOpenOrders(client):
+	orders = list(client.get_orders())
 	result = Decimal(0.00)
 	for order in orders:
 		if order["side"] == ORDER_SIDE_BUY:
 			result = result + Decimal(order["price"]) * Decimal(order["size"])
 	return result
 
-def placeOrder(client):
-	# play with XLM only to start with
-	currencyOrder = "XLM"
-	baseCurrency = "EUR" 
+def placeBuyOrder(client):
+	euroBlocked = getEuroInOpenOrders(client)
+	print (baseCurrency + " already blocked in open orders: " + str(euroBlocked))
+	euroAvailable = Decimal(getWallet(client, baseCurrency)) - euroBlocked
+	currentCurrencyPrice = Decimal(getValue(client, baseCurrency, currencyOrder))
+	buyPrice = Decimal(currentCurrencyPrice) * Decimal(BUY_PRICE_FACTOR)
+	buySize = Decimal(euroAvailable)* Decimal(0.5) / Decimal(buyPrice)
+	buyPrice = round(buyPrice,6)
+	buySize = round(buySize, 0)
+	print ("Placing an order of " + str(buySize) + " " + currencyOrder + " at price of " + str(buyPrice) + " " + baseCurrency)
+	result = client.place_limit_order(product_id=getCurrencyPair(baseCurrency, currencyOrder), 
+                  side='buy', 
+                  price=str(buyPrice), 
+                  size=str(buySize))
+	#print (result)
+	return
+
+def startTradingEngine(client):
+	currencyOrder = CRYPTO_CURRENCY
+	baseCurrency = BASE_CURRENCY 
 	i = 0
 	while i<5:
-		accounts = client.get_accounts()
 		cancelObsoleteOrders(client)
-		orders = list(client.get_orders())
-		euroBlocked = getEuroInOpenOrders(orders)
-		print (baseCurrency + " already blocked in open orders: " + str(euroBlocked))
-		euroAvailableText = getWallet(accounts, baseCurrency)
-		currentCurrencyPriceText = getValue(client, baseCurrency, currencyOrder)
-		if euroAvailableText:
-			euroAvailable = Decimal(euroAvailableText) - euroBlocked
-			if currentCurrencyPriceText:
-				currentCurrencyPrice = Decimal(currentCurrencyPriceText)
-				buyPrice = Decimal(currentCurrencyPrice) * Decimal(0.5)
-				buySize = Decimal(euroAvailable)* Decimal(0.5) / Decimal(buyPrice)
-				buyPrice = round(buyPrice,6)
-				buySize = round(buySize, 0)
-				print ("Placing an order of " + str(buySize) + " " + currencyOrder + " at price of " + str(buyPrice) + " " + baseCurrency)
-				result = client.place_limit_order(product_id=getCurrencyPair(baseCurrency, currencyOrder), 
-                              side='buy', 
-                              price=str(buyPrice), 
-                              size=str(buySize))
-				print (result)
-				print ("\n")
-				time.sleep(5)
+		placeBuyOrder(client)
+		placeSellOrder(client)
+		time.sleep(5)
 		i = i+1
 
 
@@ -126,12 +145,23 @@ def printOpenOrders(client):
 	orders = list(client.get_orders())
 	for order in orders:
 		print ("ID: " + order["id"])
-		print ("Type:" + order["side"])
+		print ("Type: " + order["side"])
 		print ("Price: " + order["price"])
 		print ("Size: " + order["size"])
 		print ("Product ID: " + order["product_id"])
 		print ("\n")
+	return
 
+def printFills(client):
+	print ("\n")
+	fills = list(client.get_fills(product_id=getCurrencyPair(BASE_CURRENCY, CRYPTO_CURRENCY)))
+	for fill in fills:
+		print ("Order ID: " + fill["order_id"])
+		print ("Type: " + fill["side"])
+		print ("Price: " + fill["price"] + " " + BASE_CURRENCY)
+		print ("Size: " + fill["size"] + " " + CRYPTO_CURRENCY)
+		print ("\n")
+	return
 
 client = None
 accounts = None
@@ -140,14 +170,16 @@ command = ""
 commandDisplayWallet = "1"
 commandDisplayOpenOrders = "2"
 commandDisplayCurrencyValues = "3"
-commandPlaceOrder = "4"
+commandStartTradingEngine = "4"
+commandDisplayFills = "5"
 commandExit = "e"
-commandList = [commandDisplayWallet, commandDisplayOpenOrders, commandDisplayCurrencyValues, commandPlaceOrder, commandExit]
+commandList = [commandDisplayWallet, commandDisplayOpenOrders, commandDisplayCurrencyValues, commandStartTradingEngine, commandDisplayFills, commandExit]
 commandMainInput = "What's next?\n" \
     "Press 1 to display your wallets\n" \
     "Press 2 to display your open orders\n" \
     "Press 3 to display current currency values\n" \
-    "Press 4 to place an order\n" \
+    "Press 4 to start the trading engine\n" \
+    "Press 5 to display recent fills\n" \
     "Press e (or ctrl+c) to exit the program\n" \
     "Select your choice: "
 API_KEY = 'APIKey'
@@ -162,21 +194,24 @@ try:
     config = getConfiguration()
     client = cbpro.AuthenticatedClient(config[API_KEY], config[API_SECRET],
                                       config[API_PASSPHRASE], api_url=api_url)
-    public_client = cbpro.PublicClient()
-    accounts = client.get_accounts()
 except Exception as exc:
     print ("Catched exception: %s" % (exc))
-if accounts:
-    while 1:
-        command = promptCommand(commandMainInput, commandList)
-        if command == commandDisplayWallet:
-            printWallets(accounts)
-        elif command == commandDisplayOpenOrders:
-        	printOpenOrders(client)
-        elif command == commandDisplayCurrencyValues:
-            printValues(client)
-        elif command == commandPlaceOrder:
-        	placeOrder(client)
-        elif command == commandExit:
-            exit()
-        print ("\n")
+if client:
+	try:
+	    while 1:
+	        command = promptCommand(commandMainInput, commandList)
+	        if command == commandDisplayWallet:
+	            printWallets(client)
+	        elif command == commandDisplayOpenOrders:
+	        	printOpenOrders(client)
+	        elif command == commandDisplayCurrencyValues:
+	            printValues(client)
+	        elif command == commandStartTradingEngine:
+	        	startTradingEngine(client)
+	        elif command == commandDisplayFills:
+	        	printFills(client)
+	        elif command == commandExit:
+	            exit()
+	        print ("\n")
+	except Exception as exc:
+		print ("Catched exception: %s" % (exc))
